7b8d0419e1fe7936201328c2b54ab01170ebcfbb
Daniel <danieljcoh@gmail.com>
2023-04-27 23:11:47 -0400

    Functionality Part 1
    - Adding JS file and convert() function
    - Changed some styling in CSS
    - Added an input field

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+EQUATION AND FORMAT
+||________________|| 
+  
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+
+20 meters = 65.616 feet | 20 feet = 6.096 meters
+20 liters = 5.284 gallons | 20 gallons = 75.708 liters
+20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
+*/
+
+// CARDS
+cardOne = document.getElementById("card-1")
+cardTwo = document.getElementById("card-2")
+cardThree = document.getElementById("card-3")
+
+inputEl = document.getElementById("input-el")
+
+function convert(){
+    alert(inputEl.value)
+}
+
